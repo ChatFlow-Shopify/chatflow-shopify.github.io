b713f6e2d44be5385a833170d3bb86f500793eee
usari14 <udogar583@gmail.com>
2026-01-02 16:57:55 +0500

    pushed

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function FeaturesPage() {
+    
     const features = [
         {
             icon: (
